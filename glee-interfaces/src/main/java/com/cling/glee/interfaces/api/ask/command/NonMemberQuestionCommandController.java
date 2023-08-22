9e6e77733043f19f7774b2ab8ce8d0a8dd33e276
sieuneee <kseun021@naver.com>
2023-08-22 23:16:19 +0900

    feat: 비회원 질문 개발

--- glee-interfaces/src/main/java/com/cling/glee/interfaces/api/ask/command/NonMemberQuestionCommandController.java
+++ glee-interfaces/src/main/java/com/cling/glee/interfaces/api/ask/command/NonMemberQuestionCommandController.java
@@ -1,9 +1,28 @@
 package com.cling.glee.interfaces.api.ask.command;
 
+import com.cling.glee.domain.service.command.AskCommandService;
+import com.cling.glee.domain.service.vo.QuestionCreateVO;
+import com.cling.glee.interfaces.api.ask.command.dto.QuestionCreateCommandDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/nonmember/question")
+@RequiredArgsConstructor
 public class NonMemberQuestionCommandController {
+
+    private final AskCommandService askCommandService;
+
+    // 질문 등록
+    @PostMapping("/register")
+    public void questionRegister(@RequestBody QuestionCreateCommandDTO questionCreateCommandDTO){
+        askCommandService.registerQuestion(QuestionCreateVO.builder()
+                .answerUser(questionCreateCommandDTO.getAnswerUser())
+                .questionContent(questionCreateCommandDTO.getQuestionContent())
+                .isMember(false)
+                .build());
+    }
 }
