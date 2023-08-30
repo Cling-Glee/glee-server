3b17f7c664918fbcec676a367913ec84e45839a6
itsme-shawn <chs98105@gmail.com>
2023-08-30 10:42:59 +0900

    etc: git ignore 적용 안 돼서 캐시 초기화

--- glee-domain/src/main/java/com/cling/glee/GleeDomainApplication.java
+++ glee-domain/src/main/java/com/cling/glee/GleeDomainApplication.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @PropertySource(ignoreResourceNotFound = true,
 		value = {
-				"classpath:application-core-local.yml",
 				"classpath:application-core-${spring.profiles.active}.yml"
 		}, factory = YamlPropertySourceFactory.class)
 public class GleeDomainApplication {
